7faa97cc9c5968aaa71f081f89a61ce23f0760e7
YurijM <y-myagkov@mail.ru>
2022-07-12 20:30:06 +0300

    Замена val на var

--- app/src/main/java/com/example/tote_test/models/GamblerModel.kt
+++ app/src/main/java/com/example/tote_test/models/GamblerModel.kt
@@ -8,7 +8,7 @@ data class GamblerModel(
     val nickname: String = "",
     val family: String = "",
     val name: String = "",
-    val gender: String = "",
+    var gender: String = "",
     val photoUrl: String = EMPTY, // для Picasso поле не должно быть пустым
     val stake: Int = 0,
     val points: Double = 0.00,
